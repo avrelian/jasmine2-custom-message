055e64ab2f39833574cef4f9b1715f3e8310f35a
Sergey Radchenko <avrelian@yandex.ru>
2014-01-13 17:14:41 +0400

    wrapIt and wrapExpect functions are secured against multiple calls and slightly simplified

--- jasmine-custom-message.js
+++ jasmine-custom-message.js
@@ -5,26 +5,26 @@
 
 
   var wrapIt = function() {
-    if (global.jasmine && global.it) {
-      global.it = (function() {
-        var it = global.it;
-        return function(desc, func, customMessages) {
+    if (global.jasmine && global.it && !global.it.wrappedForCustomMessages) {
+      global.it = function(it) {
+        var wrappedIt = function(desc, func, customMessages) {
           var spec = it(desc, func);
           if(typeof customMessages != 'object') {
             customMessages = {0: customMessages};
           }
           spec.customMessages = customMessages || {};
           return spec;
-        }
-      })();
+        };
+        wrappedIt.wrappedForCustomMessages = true;
+        return wrappedIt;
+      }(global.it);
     }
   };
 
   var wrapExpect = function() {
-    if (global.jasmine && global.expect) {
-      global.expect = (function() {
-        var expect = global.expect;
-        return function(actual) {
+    if (global.jasmine && global.expect && !global.expect.wrappedForCustomMessages) {
+      global.expect = function(expect) {
+        var wrappedExpect = function(actual) {
           var assertion = expect(actual);
           var spec = assertion.spec;
           var assertionId = spec.results_.totalCount;
@@ -36,7 +36,9 @@
           }
           return assertion;
         };
-      })();
+        wrappedExpect.wrappedForCustomMessages = true;
+        return wrappedExpect;
+      }(global.expect);
     }
   };
 
