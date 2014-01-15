3b09e4b51568638ae9f886abbd589ece0e853715
Sergey Radchenko <avrelian@yandex.ru>
2014-01-15 10:50:28 +0400

    disambigued; improved readability; added support for nested message functions; dropped automatic jasmine `it` and `expect` functions wrapping in browsers;

--- jasmine-custom-message.js
+++ jasmine-custom-message.js
@@ -1,18 +1,36 @@
-(function() {
+/*
+ * jasmine-custom-message
+ * https://github.com/avrelian/jasmine-custom-message
+ *
+ * Copyright (c) 2014 Sergey Radchenko
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+(function(undefined) {
   var global = Function('return this')();
   var isBrowserEnv = global.window && global === global.window;
   var isCommonJS = typeof module !== 'undefined' && module.exports;
 
+  var ofType = function(val) {
+    var types = [].slice.apply(arguments).slice(1);
+    var valType = val === null ? 'null' : typeof val;
+    return types.indexOf(valType) > -1;
+  };
+
 
   var wrapIt = function() {
-    if (global.jasmine && global.it && !global.it.wrappedForCustomMessages) {
+    if (global.jasmine && global.it && ! global.it.wrappedForCustomMessages) {
       global.it = (function(it) {
         var wrappedIt = function(desc, func, customMessages) {
           var spec = it(desc, func);
-          if(typeof customMessages != 'object') {
+          if (ofType(customMessages, 'function', 'string', 'number', 'boolean')) {
             customMessages = {0: customMessages};
           }
-          spec.customMessages = customMessages || {};
+          if (customMessages) {
+            spec.customMessages = customMessages;
+          }
           return spec;
         };
         wrappedIt.wrappedForCustomMessages = true;
@@ -28,10 +46,29 @@
           var assertion = expect(actual);
           var spec = assertion.spec;
           var assertionId = spec.results_.totalCount;
-          var message = spec.customMessages[assertionId];
-          if (message) {
+          var message = spec.customMessages && spec.customMessages[assertionId];
+          if (! ofType(message, 'undefined', 'null')) {
             assertion.message = function() {
-              return typeof message == 'function' ? message.apply(assertion, arguments) : message;
+              while (! ofType(message, 'string', 'number', 'boolean')) {
+                switch (true) {
+                  case ofType(message, 'undefined', 'null'):
+                    message = 'You cannot use `' + message + '` as a custom message';
+                    break;
+                  case ofType(message, 'function'):
+                    message = message.apply(assertion, arguments);
+                    break;
+                  case message && ofType(message.toString, 'function') && message.toString().indexOf('[object ') !== 0:
+                    message = message.toString();
+                    break;
+                  case JSON && ofType(JSON.stringify, 'function'):
+                    message = JSON.stringify(message);
+                    break;
+                  default:
+                    message = 'N/A';
+                }
+              }
+
+              return message;
             };
           }
           return assertion;
@@ -46,14 +83,16 @@
     wrapIt();
     wrapExpect();
   };
+  // for separate testing of jasmine without and with the module
+  init.wrapIt = wrapIt;
+  init.wrapExpect = wrapExpect;
 
   if (isBrowserEnv) {
-    init();
+    if (global.jasmine) {
+      global.jasmine.initJasmineCustomMessage = init;
+    }
   } else {
     if (isCommonJS) {
-      // for separate testing of jasmine without and with the module
-      init.wrapIt = wrapIt;
-      init.wrapExpect = wrapExpect;
       module.exports = init;
     }
   }
