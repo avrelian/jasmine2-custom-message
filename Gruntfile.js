c27d164bbe03f6beed09be0acaa9a9347df39c94
Sergey Radchenko <avrelian@yandex.ru>
2015-05-14 10:21:36 +0300

    Specs for running through protractor

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,16 +18,31 @@ module.exports = function(grunt) {
     jasmine_node: {
       // options have no effect in `grunt-jasmine-node`#v0.1.0
       test: {}
+    },
+
+    protractor: {
+      options: {
+      },
+      jasmine_custom_message: {
+        options: {
+          configFile: 'specs/protractor/conf.js',
+          args: {
+            verbose: true
+          }
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jasmine-node');
+  grunt.loadNpmTasks('grunt-protractor-runner');
 
   // `jasmine-node` does not yet support `jasmine`#v2.0.0
   // To test `jsamine-custom-message` you can use a browser
-  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('test', ['jshint','protractor']);
+
   grunt.registerTask('default', ['test']);
 
 };
