7fee7c9da027e2788de1de33fd69d79c02ea59a5
Keith Zimmerman <keithrz@gmail.com>
2018-02-23 12:14:09 -0600

    Made some updates to make this work on newer systems: * Upgraded grunt-protractor, which updates protractor to 5.x * Removed selenium address.  Use webdriver-manager, packaged with protractor, to start up selenium server. * Add `webdriver-manager update` as a pretest step.  Perhaps this should be in the grunt file instead... * Removed deprecated protractor option

--- specs/protractor/conf.js
+++ specs/protractor/conf.js
@@ -2,7 +2,6 @@ var colors = require('colors');
 colors.enabled = true;
 
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   framework: 'jasmine2',
-  specs: ['../nodejs/jasmine2-custom-message.spec.js', './test-jasmine2-custom-message.js']
+  specs: ['../nodejs/jasmine2-custom-message.spec.js', './test-jasmine2-custom-message.js'],
 };
